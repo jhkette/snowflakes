c61afd688af2783f888c542aabb4909735c16294
khenryjoe <joseph.ketterer@gmail.com>
2019-01-25 14:51:16 +0000

    more updates, looking like snow

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
 let snow = [];
 let gravity;
 
-function setup (){
+function setup() {
     createCanvas(windowWidth, windowHeight);
     gravity = createVector(0, 0.01);
 
 }
 
-function draw(){
+function draw() {
+    console.log(snow.length);
     background(0);
     snow.push(new Snowflake());
-    for (flake of snow){
+    for (flake of snow) {
+        flake.applyForce(gravity);
         flake.update();
         flake.render();
+    }
+
+    for (let i = snow.length - 1; i >= 0; i--) {
+        if (snow[i].offScreen()) {
+            snow.splice(i, 1);
+        }
     }
 }
--- snowflake.js
+++ snowflake.js
@@ -5,13 +5,21 @@ class Snowflake {
         this.pos = createVector(x, y);
         this.vel = createVector(0,5);
         this.acc = createVector();
-        this.r = random(4,8);
+        this.r = random(1,8);
+        this.terminalV = this.r;
+    }
+
+    applyForce(force){
+     let f = force.copy();
+     f = f.mult(this.r);
+     this.acc.add(f);
     }
 
     update(){
-        this.acc = gravity;
+        this.vel.limit(this.r * 0.2);
         this.vel.add(this.acc);
         this.pos.add(this.vel);
+        this.acc.mult(0);
     }
 
     render(){
@@ -19,4 +27,10 @@ class Snowflake {
         strokeWeight(this.r);
         point(this.pos.x, this.pos.y);
     }
+
+    offScreen(){
+        return (this.pos.y > height + this.r);
+    }
+
+
 }
