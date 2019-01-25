e4ae841cd2b413014f2fdabc11450ab40ce1995e
khenryjoe <joseph.ketterer@gmail.com>
2019-01-25 15:55:35 +0000

    limiting number of snowlflakes to 300

--- sketch.js
+++ sketch.js
@@ -4,22 +4,27 @@ let gravity;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     gravity = createVector(0, 0.01);
+    for (let i =0; i <300; i++){
+        let x = random(width);
+        let y = random(height);
+        snow.push(new Snowflake(x,y));
+    }
 
 }
 
 function draw() {
     console.log(snow.length);
     background(0);
-    snow.push(new Snowflake());
+    // snow.push(new Snowflake());
     for (flake of snow) {
         flake.applyForce(gravity);
         flake.update();
         flake.render();
     }
 
-    for (let i = snow.length - 1; i >= 0; i--) {
-        if (snow[i].offScreen()) {
-            snow.splice(i, 1);
-        }
-    }
+    // for (let i = snow.length - 1; i >= 0; i--) {
+    //     if (snow[i].offScreen()) {
+    //         snow.splice(i, 1);
+    //     }
+    // }
 }
--- snowflake.js
+++ snowflake.js
@@ -1,36 +1,63 @@
+function getRandomSize() {
+    let r = pow(random(0.01, 1), 5);
+    return constrain(r * 36,2,36);
+
+}
+
+
+
 class Snowflake {
-    constructor(){
+    constructor() {
         let x = random(width);
-        let y = random(-100 -10);
+        let y = random(-100 - 10);
         this.pos = createVector(x, y);
-        this.vel = createVector(0,5);
+        this.vel = createVector(0, 5);
         this.acc = createVector();
-        this.r = random(1,8);
-        this.terminalV = this.r;
+        this.r = getRandomSize();
+
+
     }
 
-    applyForce(force){
-     let f = force.copy();
-     f = f.mult(this.r);
-     this.acc.add(f);
+    applyForce(force) {
+        let f = force.copy();
+        f = (f.mult(this.r *0.9));
+        this.acc.add(f);
     }
 
-    update(){
-        this.vel.limit(this.r * 0.2);
+    randomize() {
+        let x = random(width);
+        let y = random(-100 - 10);
+        this.pos = createVector(x, y);
+        this.vel = createVector(0, 5);
+        this.acc = createVector();
+        this.r = getRandomSize();
+
+    }
+
+    update() {
         this.vel.add(this.acc);
+        this.vel.limit(this.r * 0.2);
+        if (this.vel.mag() < 1) {
+            this.vel.normalize()
+        }
+
+
+
         this.pos.add(this.vel);
         this.acc.mult(0);
+
+        if (this.pos.y > height + this.r) {
+            this.randomize();
+        }
     }
 
-    render(){
+    render() {
         stroke(255);
         strokeWeight(this.r);
         point(this.pos.x, this.pos.y);
     }
 
-    offScreen(){
-        return (this.pos.y > height + this.r);
-    }
+
 
 
 }
